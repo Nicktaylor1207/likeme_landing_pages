460ac937c12479a4482b3b7fc6df0b962518b99e
taylozac85 <taylozac85@gmail.com>
2013-08-08 19:58:06 -0700

    rename photos

--- public/javascripts/demo-photos.js
+++ public/javascripts/demo-photos.js
@@ -3,11 +3,14 @@ $(function(){
     $('#pro-modal-slide-right').on('click', function(){
         var $focusPhoto = $('#profile-modal-large-img');
         var $focusSource = $focusPhoto.attr('src');
-        alert($focusSource);
         var $sourceIndex = parseInt($focusSource.slice(12,13));
+        if (isNaN($sourceIndex)) {
+            $sourceIndex = parseInt($focusSource.slice(13,14));
+            $focusPhoto.attr('src', "images/ian10-" + ($sourceIndex + 1) + ".jpg");;
+        } else {
+            $focusPhoto.attr('src', "images/ian1-" + ($sourceIndex + 1) + ".jpg");
+        }
         
-        alert($sourceIndex);
-        // $focusPhoto.attr('src', "images/coffee1-" + ($sourceIndex + 1) + ".jpg");
     });
            
 });
