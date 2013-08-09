bece75ad45ecc7c5dee50b304f9fdf00df81d6fa
taylozac85 <taylozac85@gmail.com>
2013-08-08 20:52:43 -0700

    modify modal

--- public/javascripts/demo-photos.js
+++ public/javascripts/demo-photos.js
@@ -3,14 +3,31 @@ $(function(){
     $('#pro-modal-slide-right').on('click', function(){
         var $focusPhoto = $('#profile-modal-large-img');
         var $focusSource = $focusPhoto.attr('src');
-        var $sourceIndex = parseInt($focusSource.slice(12,13));
-        if (isNaN($sourceIndex)) {
-            $sourceIndex = parseInt($focusSource.slice(13,14));
-            $focusPhoto.attr('src', "images/ian10-" + ($sourceIndex + 1) + ".jpg");;
-        } else {
-            $focusPhoto.attr('src', "images/ian1-" + ($sourceIndex + 1) + ".jpg");
+        var $sourceIndex = parseInt($focusSource.slice(10,12));
+        if ($sourceIndex >= 41) {
+            $sourceIndex = 0;
         }
-        
+        $focusPhoto.attr('src', "images/ian" + ($sourceIndex + 1) + ".jpg");
+    });
+
+    $('#profile-modal-large-img').on('click', function(){
+        var $focusPhoto = $('#profile-modal-large-img');
+        var $focusSource = $focusPhoto.attr('src');
+        var $sourceIndex = parseInt($focusSource.slice(10,12));
+        if ($sourceIndex >= 41) {
+            $sourceIndex = 0;
+        }
+        $focusPhoto.attr('src', "images/ian" + ($sourceIndex + 1) + ".jpg");
+    });
+
+    $('#pro-modal-slide-left').on('click', function(){
+        var $focusPhoto = $('#profile-modal-large-img');
+        var $focusSource = $focusPhoto.attr('src');
+        var $sourceIndex = parseInt($focusSource.slice(10,12));
+        if ($sourceIndex <= 1) {
+            $sourceIndex = 42;
+        }
+        $focusPhoto.attr('src', "images/ian" + ($sourceIndex - 1) + ".jpg");
     });
            
 });
