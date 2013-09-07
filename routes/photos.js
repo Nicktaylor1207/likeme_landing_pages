b66c9af3d43121300190a5a51cc07019a131b153
Nick Taylor <nick.taylor1207@gmail.com>
2013-09-07 15:10:16 -0700

    add backend notebook post

--- routes/photos.js
+++ routes/photos.js
@@ -1,3 +1,5 @@
+var Email = require('../data/models/emails');
+
 module.exports = function(app) {
 
 	app.get('/photos-ny', function(req, res) {
@@ -6,6 +8,42 @@ module.exports = function(app) {
 
 	app.get('/photos-bou', function(req, res) {
     res.render('photos-bou');
+	});
+
+	app.post('/notebook', function(req, res) {
+
+		Email.findOne({email: req.session.email.email}, function(err, email) {
+			if (err) {
+				return next (err);
+			}
+			if (email) {
+				email.notebook.photos.push(req.body.image_url);
+				email.save(function(){
+					res.render('photos-ny');
+				});
+			} else {
+				res.redirect('/login');
+			}
+		});
+
+	});
+
+	app.post('/notebook1', function(req, res) {
+
+		Email.findOne({email: req.session.email.email}, function(err, email) {
+			if (err) {
+				return next (err);
+			}
+			if (email) {
+				email.notebook.photos.push(req.body.image_url);
+				email.save(function(){
+					res.render('photos-bou');
+				});
+			} else {
+				res.redirect('/login');
+			}
+		});
+
 	});
 
 };
