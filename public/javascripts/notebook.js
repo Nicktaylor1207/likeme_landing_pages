21b49c3d56f91193fb8e25274faba28d5c7528c1
taylozac85 <taylozac85@gmail.com>
2013-09-11 12:13:17 -0700

    notebook page is dynamic

--- public/javascripts/notebook.js
+++ public/javascripts/notebook.js
@@ -1,8 +1,10 @@
 $(function(){
-	$.each(photos, function(indexInArray, valueOfElement){
-		alert(valueOfElement);
-		var photoUrl = valueOfElement;
-		var photoDiv = "<div class='nb-photo-img-container' style='background-image: url('images\/aa\/ny1.jpg')'></div>" 
+	$.each(photos, function(indexInArray, photoUrl){
+		if ((indexInArray + 1)  % 3 == 0) {
+			var photoDiv = "<div class='nb-photo-img-container' id='third' style='background-image: url(" + photoUrl + ")'></div>"
+		} else {
+			var photoDiv = "<div class='nb-photo-img-container' style='background-image: url(" + photoUrl + ")'></div>"
+		}
 		var urlAnchor = "<a class='url' href=" + photoUrl + ">" + photoDiv + "</a>";
 		$('div.nb-photos-container').append("<div class='nb-photo-row'>" + urlAnchor + "</div>");
 	});
