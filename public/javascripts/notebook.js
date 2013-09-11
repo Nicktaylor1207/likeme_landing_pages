21675a5e372add95eea02a16e9f8c37b5d3af571
taylozac85 <taylozac85@gmail.com>
2013-09-11 11:48:56 -0700

    mongodump

--- public/javascripts/notebook.js
+++ public/javascripts/notebook.js
@@ -2,9 +2,8 @@ $(function(){
 	$.each(photos, function(indexInArray, valueOfElement){
 		alert(valueOfElement);
 		var photoUrl = valueOfElement;
-		var urlAnchor = "<a class='url' href=" + photoUrl + ">link</a>";
-		$('div.nb-photos-container').html("<div class='nb-photo-row'></div>");
-		$('div.nb-photo-row').html(urlAnchor);
-		$('a.url').html("<div class='nb-photo-img-container' style='background-image: url(" + photoUrl + ")'></div>");
+		var photoDiv = "<div class='nb-photo-img-container' style='background-image: url('images\/aa\/ny1.jpg')'></div>" 
+		var urlAnchor = "<a class='url' href=" + photoUrl + ">" + photoDiv + "</a>";
+		$('div.nb-photos-container').append("<div class='nb-photo-row'>" + urlAnchor + "</div>");
 	});
 })
