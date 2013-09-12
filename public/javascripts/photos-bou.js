090884a9d5ea56223c0cae2618a8773aec662af7
taylozac85 <taylozac85@gmail.com>
2013-09-11 17:58:40 -0700

    updated photos-bou and added pagination

--- public/javascripts/photos-bou.js
+++ public/javascripts/photos-bou.js
@@ -8,4 +8,35 @@ $(function(){
 		$.cookie("scroll-bou", $(document).scrollTop(), { expires: 1 } );
 	});
 
-})
+});
+
+$(function() {
+  $('#photos-pagination').pagination({
+      items: 50,
+      itemsOnPage: 10,
+      cssStyle: 'dark-theme',
+      currentPage: $.cookie("currPageBou"),
+      onPageClick: function(pageNumber){
+      	setPage(pageNumber, function(){
+          $(document).scrollTop(0); // not working
+        });
+      }
+  });
+
+	function setPage(pageNumber, callback) {
+    var page = "#page-" + pageNumber;
+		$('.selection').hide();
+		$(page).show();
+    callback();
+  };
+
+  var displayPage = "#page-" + $.cookie("currPageBou");
+  $('.selection').hide();
+  $(displayPage).show();
+
+  $(window).on('unload', function(){
+  	var currPage = $('#photos-pagination').pagination('getCurrentPage');
+  	$.cookie("currPageBou", currPage);
+  });
+
+});
--- public/javascripts/photos-ny.js
+++ public/javascripts/photos-ny.js
@@ -9,3 +9,32 @@ $(function(){
 	});
 
 });
+
+$(function() {
+  $('#photos-pagination').pagination({
+      items: 50,
+      itemsOnPage: 10,
+      cssStyle: 'dark-theme',
+      onPageClick: function(pageNumber){
+      	setPage(pageNumber);
+      },
+      currentPage: $.cookie("currPageNY")
+  });
+
+	function setPage(pageNumber) {
+		var page = "#page-" + pageNumber;
+		$('.selection').hide()
+		$(page).show()
+		$(document).scrollTop(0);
+  };
+
+  var displayPage = "#page-" + $.cookie("currPageNY");
+  $('.selection').hide();
+  $(displayPage).show();
+
+  $(window).on('unload', function(){
+  	var currPage = $('#photos-pagination').pagination('getCurrentPage');
+  	$.cookie("currPageNY", currPage);
+  });
+
+});
