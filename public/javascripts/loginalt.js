e3d685fca6cc6c10e1ca36d5012a9b6f705193fa
taylozac85 <taylozac85@gmail.com>
2013-09-25 11:12:09 -0700

    Add forgot password and alt login. Before adding mailer module

---
+++ public/javascripts/loginalt.js
@@ -0,0 +1,36 @@
+$(function(){
+	  
+	(function(){
+	  $('#alt-signup-form').submit(function() {
+	    function failValidation(msg) {
+	      alert(msg); // just an alert for now but you can spice this up later
+	      return false;
+	    };
+
+	    var firstName = $('#signup-first-name-input');
+	    if (firstName.val() == "") {
+	      return failValidation('Please enter your first name to sign up.');
+	    }
+
+	    var lastName = $('#signup-last-name-input');
+	    if (lastName.val() == "") {
+	      return failValidation('Please enter your last name to sign up.');
+	    }
+
+	    var email = $('#signup-email-input');
+	    if (email.val() == "") {
+	      return failValidation('Please enter your email address to sign up.');
+	    }
+
+	    var password = $('#signup-password-input');
+	    if (password.val() == "") {
+	      return failValidation('Please enter a password to sign up.');
+	    }
+
+	    return true;
+	    return false; // prevent form submitting anyway - remove this in your environment
+	  });
+	}).call(this);
+
+});
+
