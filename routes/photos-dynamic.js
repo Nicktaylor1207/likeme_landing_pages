af2773cbcfc68b1aaaae98712d1673155460b086
taylozac85 <taylozac85@gmail.com>
2013-10-24 18:43:09 -0700

    Update nav

--- routes/photos-dynamic.js
+++ routes/photos-dynamic.js
@@ -14,40 +14,9 @@ module.exports = function(app) {
 	/* Refactor -- create array of tags and run an each function for below get routes */
 	/* Add ability to get by style and type */
 
-	// function getDynPhotos(req, res, style) {
-	// 	req.session.tag = style;
- //    Photo.find({ style: style}, function(err, results){
- //    	var sorted = sortByKey(results, 'position'); // change to timestamp
- //    	if (req.session.email) {
-	//     	Email.findOne({email: req.session.email.email}, function(err, user) {
-	// 				if (err) {
-	// 					return next (err);
-	// 				}
-	// 				if (user) {
-	// 					res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-	// 				}
-	// 			});	
-	// 		} else {
-	// 			res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-	// 		}
-	// 	});
-	// }
-
-	// // var styleTags = ['cont', 'ecce'];
-
-	// // for (var i = 0; i < styleTags.length; i++) {
-	// //   app.get('/photos-' + styleTags[i], selectNav, function(req, res) {
- // //      getDynPhotos(req, res, styleTags[i]);
- // //  	});
-	// // }  
-
-	// app.get('/photos-cont', selectNav, function(req, res) {
- //    getDynPhotos(req, res, 'cont');
-	// });
-
-	app.get('/photos-ecce', selectNav, function(req, res) {
-    req.session.tag = 'ecce';
-    Photo.find({ style: 'ecce'}, function(err, results){
+	function getDynPhotos(req, res, style) {
+		req.session.tag = style;
+    Photo.find({ style: style}, function(err, results){
     	var sorted = sortByKey(results, 'position'); // change to timestamp
     	if (req.session.email) {
 	    	Email.findOne({email: req.session.email.email}, function(err, user) {
@@ -62,120 +31,97 @@ module.exports = function(app) {
 				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
 			}
 		});
+	}
+
+	function getDynPhotosType (req, res, type) {
+		req.session.tag = type;
+    Photo.find({ type: type}, function(err, results){
+    	var sorted = sortByKey(results, 'position'); // change to timestamp
+    	if (req.session.email) {
+	    	Email.findOne({email: req.session.email.email}, function(err, user) {
+					if (err) {
+						return next (err);
+					}
+					if (user) {
+						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
+					}
+				});	
+			} else {
+				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
+			}
+		});
+	}
+
+	app.get('/photos-cont', selectNav, function(req, res) {
+    getDynPhotos(req, res, 'cont');
+	});
+
+	app.get('/photos-ecce', selectNav, function(req, res) {
+    getDynPhotos(req, res, 'ecce');
 	});
 
 	app.get('/photos-mini', selectNav, function(req, res) {
-    req.session.tag = 'mini';
-    Photo.find({ style: 'mini'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'mini');
 	});
 
 	app.get('/photos-rust', selectNav, function(req, res) {
-    req.session.tag = 'rust';
-    Photo.find({ style: 'rust'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'rust');
 	});
 
 	app.get('/photos-trad', selectNav, function(req, res) {
-    req.session.tag = 'trad';
-    Photo.find({ style: 'trad'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'trad');
 	});
 
 	app.get('/photos-hip', selectNav, function(req, res) {
-    req.session.tag = 'hip';
-    Photo.find({ style: 'hip'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'hip');
 	});
 
 	app.get('/photos-indu', selectNav, function(req, res) {
-    req.session.tag = 'indu';
-    Photo.find({ style: 'indu'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'indu');
 	});
 
 	app.get('/photos-cons', selectNav, function(req, res) {
-    req.session.tag = 'cons';
-    Photo.find({ style: 'cons'}, function(err, results){
-    	var sorted = sortByKey(results, 'position'); // change to timestamp
-    	if (req.session.email) {
-	    	Email.findOne({email: req.session.email.email}, function(err, user) {
-					if (err) {
-						return next (err);
-					}
-					if (user) {
-						res.render('photos-dyn', {user: user, photos: sorted, navLogin: req.body.navLogin});
-					}
-				});	
-			} else {
-				res.render('photos-dyn', {user: false, photos: sorted, navLogin: req.body.navLogin});
-			}
-		});
+    getDynPhotos(req, res, 'cons');
 	});
+
+	app.get('/photos-full', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'full');
+	});
+
+	app.get('/photos-fron', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'fron');
+	});
+
+	app.get('/photos-shac', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'shac');
+	});
+
+	app.get('/photos-chan', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'chan');
+	});
+
+	app.get('/photos-disp', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'disp');
+	});
+
+	app.get('/photos-ligh', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'ligh');
+	});
+
+	app.get('/photos-merc', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'merc');
+	});
+
+	app.get('/photos-mann', selectNav, function(req, res) {
+    getDynPhotosType (req, res, 'mann');
+	});
+
+	// var styleTags = ['cont', 'ecce'];
+
+	// for (var i = 0; i < styleTags.length; i++) {
+	//   app.get('/photos-' + styleTags[i], selectNav, function(req, res) {
+ //      getDynPhotos(req, res, styleTags[i]);
+ //  	});
+	// }  
 
 };
