78d03ed845365ce7a74ceab408c3ea0f407fb428
taylozac85 <taylozac85@gmail.com>
2013-12-04 18:36:34 -0800

    Dynamically add photos to notebooks in notebook-create.jade

--- public/javascripts/create-notebook.js
+++ public/javascripts/create-notebook.js
@@ -3,14 +3,31 @@ $(function(){
 	$('#create-nb-modal-form').on('submit', function(){
 		
 		$('#create-nb-add-photos-modal').modal('hide');
+		var photoUrl = $('#photo-link').val();
 		
-		/* append the photo */
-		var newPhoto = "<div class='create-notebook-photo' style='background-image: url(images/store_fronts/10.jpg)'></div>"
-		$('#create-nb-add-photos-container').append(newPhoto);
+		/* Update the photoCounter */
+		var photoCounter = $('#photoCounter');
+		var count = parseInt(photoCounter.val()) + 1;
+		photoCounter.attr('value', count);
 
+		/* Append the photo input */
+		var newPhotoInput = "<input class='create-nb-photo-input hide' name='url" + count + "' value=" + photoUrl + "></input>";
+		$('#create-nb-add-photos-container').append(newPhotoInput);
+
+		/* Append the photo */
+		var newPhotoDiv = "<div class='create-notebook-photo' style='background-image: url(" + photoUrl + ")'></div>";
+		$('#create-nb-add-photos-container').append(newPhotoDiv);
+
+		$('#photo-link').val('');
 		return false; // avoid executing submit of the form
 	});
 
+});
 
+$(function(){
+
+	// $('#create-nb-form').on('submit', function(){
+	// 	$('#photoCounter').val(0);
+	// });
 
 });
